b48954401bde715ac74f9fae024e78273b8f26d3
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2020-05-26 09:44:03 -0400

    Added more code to investigate functionality within.

--- GooseNest/NestMenu.cs
+++ GooseNest/NestMenu.cs
@@ -10,7 +10,7 @@ namespace GooseNest
         private List<string> _currentLoadedScenes;
 
         private List<GOData> _lastClickedObjects;
-        private List<Object> _collectiveObjects;
+        private List<GameObject> _collectiveObjects;
 
         private Vector2 _objectsScrollPosition;
         private Vector2 _componentScrollPosition;
@@ -55,7 +55,7 @@ namespace GooseNest
             _screenTextDisplay.SetStepDirection(StepDirection.Reverse);
             _currentLoadedScenes = new List<string>();
             _lastClickedObjects = new List<GOData>();
-            _collectiveObjects = new List<Object>();
+            _collectiveObjects = new List<GameObject>();
         }
 
         public void Update()
@@ -87,11 +87,36 @@ namespace GooseNest
                     _lastClickedObjects.Clear();
                     _collectiveObjects.Clear();
 
-                    UnityEngine.Object[] allObjects = GameObject.FindObjectsOfType(typeof(GameObject));
+                    UnityEngine.GameObject[] allObjects = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
 
                     for (int index = 0; index < allObjects.Length; index++)
                     {
                         _collectiveObjects.Add(allObjects[index]);
+                    }
+                }
+            }
+            else
+            {
+                Scene baseScene = SceneManager.GetSceneByName("base");
+
+                if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.I))
+                {
+                    _collectiveObjects.Clear();
+                    if (baseScene.IsValid())
+                    {
+                        List<GameObject> rootChildren = new List<GameObject>();
+                        baseScene.GetRootGameObjects(rootChildren);
+
+                        while(rootChildren.Count > 0)
+                        {
+                            GameObject rootObject = rootChildren[0];
+                            rootChildren.RemoveAt(0);
+                            _collectiveObjects.Add(rootObject);
+                            for(int childIndex = 0; childIndex < rootObject.transform.childCount; childIndex++)
+                            {
+                                rootChildren.Add(rootObject.transform.GetChild(childIndex).gameObject);
+                            }
+                        }
                     }
                 }
             }
@@ -100,6 +125,8 @@ namespace GooseNest
         public void OnGUI()
         {
             _screenTextDisplay.Clear();
+            _screenTextDisplay.SetStepDirection(StepDirection.Reverse);
+
             int screenWidth = Screen.width, screenHeight = Screen.height;
 
             _screenTextDisplay.AddText("Goose Nest v0.1");
@@ -108,11 +135,6 @@ namespace GooseNest
             _screenTextDisplay.AddText(string.Format("Scenes loaded: {0}", SceneManager.sceneCount));
             _screenTextDisplay.AddText(string.Format("Active scene: {0}", SceneManager.GetActiveScene().name));
 
-            //for(int sceneIndex = 0; sceneIndex < SceneManager.sceneCount; sceneIndex++)
-            //{
-            //    _screenTextDisplay.AddText(string.Format("Loaded scene name: {0}", SceneManager.GetSceneAt(sceneIndex).name));
-            //}
-
             for (int sceneIndex = 0; sceneIndex < SceneManager.sceneCountInBuildSettings; sceneIndex++)
             {
                 _screenTextDisplay.AddText(string.Format("Built scene path: {0}", SceneUtility.GetScenePathByBuildIndex(sceneIndex)));
@@ -120,6 +142,34 @@ namespace GooseNest
 
             if (GUI.Button(new Rect((screenWidth - 300), 110, 80, 30), "Load Game"))
             {
+                GameObject mainMenu = GameObject.Find("MainMenu");
+                GameObject mainMenuChild = null;
+                if(mainMenu)
+                {
+                    if(!mainMenu.transform.parent)
+                    {
+                        int menuChildCount = mainMenu.transform.childCount;
+
+                        for(int menuIndex = 0; menuIndex < menuChildCount; menuIndex++)
+                        {
+                            GameObject menuChild = mainMenu.transform.GetChild(menuIndex).gameObject;
+
+                            if(menuChild.name.Equals("MainMenu", System.StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                mainMenuChild = menuChild;
+                                break;
+                            }
+                        }
+                    }
+
+                    mainMenu.SetActive(false);
+
+                    if (mainMenuChild)
+                    {
+                        mainMenuChild.SetActive(false);
+                    }
+                }
+
                 SceneManager.LoadScene("main", LoadSceneMode.Additive);
             }
 
@@ -128,7 +178,33 @@ namespace GooseNest
                 SceneManager.LoadScene("loading");
             }
 
-            //_screenTextDisplay.SetPosition(new Vector2(20, screenHeight - 100));
+            _screenTextDisplay.SetPosition(new Vector2(20, 100));
+            _screenTextDisplay.SetStepDirection(StepDirection.Normal);
+
+            _screenTextDisplay.AddText(string.Format("Camera position: {0}", Camera.current.gameObject.transform.position));
+
+            try
+            {
+                GameObject menuParent = GameObject.Find("Menus");
+
+                if (menuParent)
+                {
+                    RectTransform rectTransform = menuParent.GetComponent<RectTransform>();
+
+                    if (rectTransform)
+                    {
+                        _screenTextDisplay.AddText(string.Format("Menus position: {0}", rectTransform.position));
+                    }
+                }
+                else
+                {
+                    _screenTextDisplay.AddText(string.Format("Couldn't find the menus"));
+                }
+            }
+            catch(System.Exception e)
+            {
+                _screenTextDisplay.AddText("Couldn't find menu: " + e.Message);
+            }
 
             float listLength = 0;
             if (_lastClickedObjects.Count > 0)
@@ -152,7 +228,7 @@ namespace GooseNest
 
                 for (int index = 0; index < _lastClickedObjects.Count; index++)
                 {
-                    if(GUI.Button(new Rect(viewPosition, viewSize), string.Format("{0} : {1}", _lastClickedObjects[index].Object, _lastClickedObjects[index].Object.GetComponents<Component>().Length)))
+                    if(GUI.Button(new Rect(viewPosition, viewSize), string.Format("{0} : {1} {2}", _lastClickedObjects[index].Object, _lastClickedObjects[index].Object.GetComponents<Component>().Length, _lastClickedObjects[index].Object.activeSelf ? "++" : "--")))
                     {
                         focusedObject = _lastClickedObjects[index].Object;
                         _componentScrollPosition = Vector2.zero;
@@ -163,9 +239,9 @@ namespace GooseNest
 
                 for (int index = 0; index < _collectiveObjects.Count; index++)
                 {
-                    if(GUI.Button(new Rect(viewPosition, viewSize), string.Format("{0}", _collectiveObjects[index].name)))
+                    if(GUI.Button(new Rect(viewPosition, viewSize), string.Format("{0} {1}", _collectiveObjects[index].name, _collectiveObjects[index].activeSelf ? "++" : "--")))
                     {
-                        focusedObject = _collectiveObjects[index] as GameObject;
+                        focusedObject = _collectiveObjects[index];
                         _componentScrollPosition = Vector2.zero;
                     }
                     viewPosition += viewStep;
@@ -186,17 +262,54 @@ namespace GooseNest
                     componentListLength = components.Length * 24;
                 }
 
-                _componentScrollPosition = GUI.BeginScrollView(new Rect(550, screenHeight - 350, 400, 300), _componentScrollPosition, new Rect(0, 0, 420, componentListLength));
+                _componentScrollPosition = GUI.BeginScrollView(new Rect(550, screenHeight - 350, 400, 300), _componentScrollPosition, new Rect(0, 0, 420f * 2f, componentListLength * 30f));
 
                 Vector2 viewPosition = new Vector2(0f, 0f);
-                Vector2 viewStep = new Vector2(0f, 24f);
+                Vector2 viewStep = new Vector2(0f, 26f);
                 Vector2 viewSize = new Vector2(400f, 24f);
 
                 for(int index = 0; index < components.Length; index++)
                 {
-                    GUI.Label(new Rect(viewPosition, viewSize), string.Format("{0} - {1}", components[index].name, components[index].GetType()));
+                    Component component = components[index];
+                    GUI.Label(new Rect(viewPosition, viewSize), string.Format("{0} - {1}", component.name, component.GetType()));
                     viewPosition += viewStep;
+
+                    System.Reflection.MemberInfo[] compMembers = component.GetType().GetMembers();
+                    System.Reflection.FieldInfo[] compFields = component.GetType().GetFields();
+                    System.Reflection.MethodInfo[] compMethods = component.GetType().GetMethods();
+                    if (component.GetType().Name.Equals("MouseableMenuCollider", System.StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        foreach(System.Reflection.FieldInfo field in compFields)
+                        {
+                            GUI.Label(new Rect(viewPosition, viewSize * 2f), string.Format("{0} : {1}", field.GetValue(component).GetType(), field.GetValue(component)));
+                            viewPosition += viewStep;
+                        }
+                    }
+
+                    if (component.GetType().Name.Equals("EraseSelectMenu", System.StringComparison.InvariantCultureIgnoreCase) 
+                        /*|| component.GetType().Name.Equals("SaveMenu", System.StringComparison.InvariantCultureIgnoreCase)*/)
+                    {
+                        foreach (System.Reflection.MethodInfo method in compMethods)
+                        {
+                            GUI.Label(new Rect(viewPosition, viewSize * 2f), string.Format("{0}", method.Name));
+                            viewPosition += viewStep;
+                        }
+                    }
                 }
+
+                if(GUI.Button(new Rect(viewPosition, viewSize), "Activate"))
+                {
+                    focusedObject.SetActive(!focusedObject.activeSelf);
+                }
+                viewPosition += viewStep;
+
+                GUI.Label(new Rect(viewPosition, viewSize), string.Format("Tag: {0}", focusedObject.tag));
+                viewPosition += viewStep;
+
+                GUI.Label(new Rect(viewPosition, viewSize), string.Format("Parent: {0}", focusedObject.transform.parent ? focusedObject.transform.parent.gameObject.name : "No parent"));
+                viewPosition += viewStep;
+
+                GUI.EndScrollView();
             }
 
             _screenTextDisplay.DrawTextToGUI();
